75fb9a1aaf444de7a36694141660ec95d2aa77ad
soppari-akshaya <soppariakshaya@gmail.com>
2025-07-19 15:11:49 +0530

    Add files via upload
    updated the functions file

---
+++ functions1.js
@@ -0,0 +1,12 @@
+//DOM stands for document object model
+//by using DOM it have two operations i.e. selecting and manipulating HTML.
+// console.dir(document)
+// console.dir(document.all)
+
+// console.dir(document.all)
+// document.all[6].innerText="Hello Akshaya";
+
+console.dir(document.all)  //HTMLAllCollection(15)
+document.getElementById("img").id  //'img'
+document.getElementById("img").src //'file:///C:/Users/Akshaya/OneDrive/Pictures/Screenshots/Screenshot%202025-07-05%20120512.png'
+document.getElementById("img").alt  //'image1'
